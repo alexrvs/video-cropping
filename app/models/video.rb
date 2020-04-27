bf6def2d98beb7443c517ef059e81fe0e7be3db0
alexrvs <vasyutin4@gmail.com>
2020-04-27 13:21:28 +0300

    CarrierWave backgrounder

--- app/models/video.rb
+++ app/models/video.rb
@@ -6,4 +6,15 @@ class Video
   field :file_processing, type: Mongoid::Boolean
   field :start_time_trim, type: Integer
   field :end_time_trim, type: Integer
+
+  belongs_to :user, inverse_of: :video
+
+  mount_uploader :file, VideoUploader, mount_on: :file
+
+  process_in_background :file
+
+  store_in_background :file
+
+  validates_presence_of :file
+
 end
