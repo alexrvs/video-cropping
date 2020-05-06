48585bd11ebadf5f488f96554998eec5fd9890a5
alexrvs <vasyutin4@gmail.com>
2020-05-06 17:27:34 +0300

    Swagger fixes

--- Gemfile
+++ Gemfile
@@ -48,6 +48,7 @@ group :development, :test do
   gem 'faker'
   gem 'factory_bot_rails'
   gem 'database_cleaner'
+  gem 'rswag'
 end
 
 gem 'aasm', '~> 4.11', '>= 4.11.1'
@@ -57,7 +58,7 @@ group :test do
   gem 'mongoid-rspec'
   gem 'simplecov'
   gem 'simplecov-rcov'
-  gem 'rswag'
+
 end
 
 group :development do
--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -24,10 +24,10 @@ RSpec.configure do |config|
       paths: {},
       servers: [
         {
-          url: 'http://localhost:3000/',
+          url: 'https://videotrim.herokuapp.com/',
           variables: {
             defaultHost: {
-              default: 'localhost:3000'
+              default: 'https://videotrim.herokuapp.com'
             }
           }
         }
