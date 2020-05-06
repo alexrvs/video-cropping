923c60b4a8566152be8eebc5c2f84427ee5e9a67
alexrvs <vasyutin4@gmail.com>
2020-05-06 03:00:23 +0300

    Swagger docs

--- spec/integration/videos_spec.rb
+++ spec/integration/videos_spec.rb
@@ -4,24 +4,42 @@ require 'swagger_helper'
 describe 'Video API' do
 
   path '/api/v1/videos' do
-    parameter name: :video, in: :formData, schema: {
-      type: :object,
-      properties: {
-        start_time_trim: { type: :integer },
-        end_time_trim: { type: :integer }
-      },
-      required: [ 'start_time_trim', 'end_time_trim' ]
-    }
-    post 'Uploads a user video' do
+    post 'Uploads videos by authorized user' do
       tags 'Videos'
-      description
-      consumes 'multipart/form-data'
-      parameter name: :video, in: :formData, type: :file, required: true
-      response '200', 'video created' do
-        let(:file) { Rack::Test::UploadedFile.new(Rails.root.join("spec/fixtures/videos/nature.mp4")) }
-          #run_test!
+      consumes 'application/json'
+      parameter name: 'Authorization', in: :header, type: :string, default: 'Token 8b6a04aac0264bef89d99c3cc5fd8513'
+      parameter name: :video, in: :fromData, schema: {
+        type: :object,
+        properties: {
+          start_time_trim: { type: :integer, example: 10 },
+          end_time_trim: { type: :integer, example: 50 }
+        },
+        required: %w[start_time_trim end_time_trim]
+      }
+      parameter name: :input_video, in: :formData, type: :file, required: true
+
+      response '401', :unauthorized do
+        let(:Authorization){ '' }
+        let(:video) {
+          {
+            start_time_trim: 10,
+            end_time_trim: 50,
+            input_video: Rack::Test::UploadedFile.new(Rails.root.join("spec/fixtures/videos/nature.mp4"))
+          }
+        }
+        run_test!
       end
+
+      response '201', :created do
+        let!(:user) { FactoryBot.create(:user) }
+        let(:Authorization) { 'Token ' + user.access_token }
+        let(:video) { { start_time_trim: 10, end_time_trim: 50, input_video: Rack::Test::UploadedFile.new(Rails.root.join("spec/fixtures/videos/nature.mp4")) } }
+        run_test!
+      end
+
     end
   end
+
+
 
 end
